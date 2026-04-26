f6cf9a85424ff0b37bb998e233589fb061a2b11b
Kalleb Abreu <Kalleb.abreu@proton.me>
2026-04-26 20:13:54 -0300

    feat(config): add config.yml and src/config.R for pipeline configuration

---
+++ src/config.R
@@ -0,0 +1,13 @@
+library(config)
+
+#' Load pipeline configuration from config.yml
+#'
+#' Respects R_CONFIG_ACTIVE environment variable (defaults to "default").
+load_config <- function() {
+  config::get(file = here::here("config.yml"))
+}
+
+#' Flatten the grouped resamplings list into a character vector
+all_resamplings <- function(cfg) {
+  unlist(cfg$resamplings, use.names = FALSE)
+}
